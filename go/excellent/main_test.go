99b9bc6f80d5df7f2cfa3e61613bbf6a51ded527
suzukimit <mtr.suzuki@gmail.com>
2024-12-29 20:16:28 +0900

    small fix

--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestEvenOrOdd(t *testing.T) {
-	result := EvenOrOdd(10)
+	result := EvenOrOdd(100)
 	if result != "even" {
 		t.Errorf("EvenOrOdd(10) = %s; want even", result)
 	}
